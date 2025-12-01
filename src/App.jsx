08612a3d8f391d8b79b98e8677f0c4a456002296
Himanshi Sharma <himashi2525sharma@gmail.com>
2025-12-01 15:46:18 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Home from "./pages/Home"; // Adjust path based on your folder structure
 
 function App() {
-  
-
   return (
-    <>
-      <div>hello</div>
-    </>
-  )
+    <div>
+      <Home /> {/* Render your Home component here */}
+    </div>
+  );
 }
 
-export default App
+export default App;
